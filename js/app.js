1acd55616e8d11ed9e638dd5683166f73eb48b60
ripplefox <lr@ripplefox.com>
2020-05-01 23:18:57 +0800

    update lang and tutorial

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 var shApp = angular.module('foxhome', ['pascalprecht.translate']);
 
 shApp.config(['$translateProvider', function($translateProvider) {
-        $translateProvider.translations('cn', translate_cn);
+  $translateProvider.translations('cn', translate_cn);
 	$translateProvider.translations('en', translate_en);
-	//$translateProvider.translations('jp', translate_jp);
-	//$translateProvider.translations('ko', translate_ko);
-	$translateProvider.preferredLanguage('en');
+	$translateProvider.preferredLanguage('cn');
 	$translateProvider.useSanitizeValueStrategy('escape');
 }]);
 
 shApp.run(['$rootScope', '$window', '$translate',function($rootScope, $window, $translate) {
-	let key = $window.localStorage['lang'] || 'en';
+	let key = $window.localStorage['lang'] || 'cn';
 	$translate.use(key);
+	$rootScope.lang = $translate.use();
 }]);
 
 shApp.factory('SettingFactory', function($window) {
@@ -20,15 +19,16 @@ shApp.factory('SettingFactory', function($window) {
 			$window.localStorage['lang'] = lang;
 		},
 		getLang : function() {
-			return $window.localStorage['lang'] || 'en';
+			return $window.localStorage['lang'] || 'cn';
 		}
 	};
 });
-shApp.controller("LangCtrl", [ '$scope', '$translate', 'SettingFactory',
-  function($scope, $translate, SettingFactory) {
+shApp.controller("LangCtrl", [ '$scope', '$rootScope', '$translate', 'SettingFactory',
+  function($scope, $rootScope, $translate, SettingFactory) {
+  $rootScope.lang = $translate.use();
 	$scope.changeLanguage = function (key) {
-
-        $translate.use(key);
+      $translate.use(key);
+      $rootScope.lang = $translate.use();
 	    SettingFactory.setLang(key);
 	};
 }]);
