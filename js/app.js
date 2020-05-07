d23377e1259558857a119a458f687088dfefb55d
ripplefox <lr@ripplefox.com>
2020-05-08 00:33:13 +0800

    history ready

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ shApp.factory('SettingFactory', function($window) {
 		}
 	};
 });
+
 shApp.controller("LangCtrl", [ '$scope', '$rootScope', '$translate', 'SettingFactory',
   function($scope, $rootScope, $translate, SettingFactory) {
   $rootScope.lang = $translate.use();
@@ -33,5 +34,39 @@ shApp.controller("LangCtrl", [ '$scope', '$rootScope', '$translate', 'SettingFac
 	};
 }]);
 
+shApp.controller("XlmGatewayCtrl", [ '$scope', '$rootScope', '$http',
+  function($scope, $rootScope, $http) {
+  $scope.deposit = [];
+  $scope.withdraw = [];
+  
+  $http({
+    method: 'GET',
+    url: "https://ripplefox.com/data/history/stellar2ripple"
+  }).then(function(res) {
+    console.log(res.data);
+    $scope.deposit = res.data.data;
+  }).catch(err => {
+    console.log('ignore version check', err);
+  });
+  
+  $http({
+    method: 'GET',
+    url: "https://ripplefox.com/data/history/ripple2stellar"
+  }).then(function(res) {
+    console.log(res.data);
+    $scope.withdraw = res.data.data;
+  }).catch(err => {
+    console.log('ignore version check', err);
+  });
+}]);
 
-
+shApp.filter('shortaddress', function() {
+  return function(address) {
+    if (!address || address.length < 8) {
+      return address;
+    }
+    var start = address.substring(0, 4);
+    var end = address.substring(address.length - 4);
+    return start + '...' + end;
+  }
+});
